b24290d409a128026796ccce232301956d507893
Shiv Raj Dangi <shiv@codingcafe.website>
2020-11-11 12:44:50 +0530

    changed the flook and feel of login page

--- resources/views/frontend/layouts/default.blade.php
+++ resources/views/frontend/layouts/default.blade.php
@@ -27,127 +27,7 @@
 
   <!-- Template Main CSS File -->
   <link href="assets/css/style.css" rel="stylesheet">
-  <style>
-
-/* Full-width input fields */
-input[type=text], input[type=password] {
-  width: 100%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  display: inline-block;
-  border: 1px solid #ccc;
-  box-sizing: border-box;
-}
-
-/* Set a style for all buttons */
-button {
-  background-color: #4CAF50;
-  color: white;
-  padding: 14px 20px;
-  margin: 8px 0;
-  border: none;
-  cursor: pointer;
-  width: 100%;
-}
-
-button:hover {
-  opacity: 0.8;
-}
-
-/* Extra styles for the cancel button */
-.cancelbtn {
-  width: auto;
-  padding: 10px 18px;
-  background-color: #f44336;
-}
-
-/* Center the image and position the close button */
-.imgcontainer {
-  text-align: center;
-  margin: 24px 0 12px 0;
-  position: relative;
-}
-
-img.avatar {
-  width: 40%;
-  border-radius: 50%;
-}
-
-.container {
-  padding: 16px;
-}
-
-span.psw {
-  float: right;
-  padding-top: 16px;
-}
-
-/* The Modal (background) */
-.modal {
-  display: none; /* Hidden by default */
-  position: fixed; /* Stay in place */
-  z-index: 1; /* Sit on top */
-  left: 0;
-  top: 0;
-  width: 100%; /* Full width */
-  height: 100%; /* Full height */
-  overflow: auto; /* Enable scroll if needed */
-  background-color: rgb(0,0,0); /* Fallback color */
-  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
-  padding-top: 60px;
-}
-
-/* Modal Content/Box */
-.modal-content {
-  background-color: #fefefe;
-  margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
-  border: 1px solid #888;
-  width: 80%; /* Could be more or less, depending on screen size */
-}
-
-/* The Close Button (x) */
-.close {
-  position: absolute;
-  right: 25px;
-  top: 0;
-  color: #000;
-  font-size: 35px;
-  font-weight: bold;
-}
-
-.close:hover,
-.close:focus {
-  color: red;
-  cursor: pointer;
-}
-
-/* Add Zoom Animation */
-.animate {
-  -webkit-animation: animatezoom 0.6s;
-  animation: animatezoom 0.6s
-}
-
-@-webkit-keyframes animatezoom {
-  from {-webkit-transform: scale(0)} 
-  to {-webkit-transform: scale(1)}
-}
   
-@keyframes animatezoom {
-  from {transform: scale(0)} 
-  to {transform: scale(1)}
-}
-
-/* Change styles for span and cancel button on extra small screens */
-@media screen and (max-width: 300px) {
-  span.psw {
-     display: block;
-     float: none;
-  }
-  .cancelbtn {
-     width: 100%;
-  }
-}
-</style>
 </head>
 <body>
 
@@ -269,7 +149,7 @@ span.psw {
         <div class="container">
           <h2>Get Professional Technicians</h2>
           <p>here will be text </p>
-          <!-- <a href="{{url('startup-consumer')}}"> --><button onclick="document.getElementById('id01').style.display='block'" style="width:auto;" class="btn btn-button bg-warning">Hire a Technician</button><!-- </a> -->
+         <button data-toggle="modal" data-target="#customer_login" class="btn btn-button bg-warning">Hire a Technician</button>
         </div>
       </div>
     </div>
@@ -278,70 +158,113 @@ span.psw {
         <div class="container">
           <h2>Get a Business</h2>
           <p>here will be text </p>
-          <!-- <a href="{{url('startup-consumer')}}"> --><button onclick="document.getElementById('technician').style.display='block'" style="width:auto;" class="btn btn-button bg-warning">Create Technician Account</button><!-- </a> -->
+          <button data-toggle="modal" data-target="#technician_login" class="btn btn-button bg-warning">Create Technician Account</button>
 
         </div>
       </div>
     </div>
   </div>
 </div>
-<!--  Cosnumer login     -->
-<div id="id01" class="modal" style="">
-  <form class="modal-content animate" action="/action_page.php" method="post">
-    <div class="imgcontainer">
-      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
-      <img src="img_avatar2.png" alt="Avatar" class="avatar">
-      <h2>Get Professional Technicians</h2>
-    </div>
-    <div class="container">
-      <label for="uname"><b>Username</b></label>
-      <input type="text" placeholder="Enter Username" name="uname" required>
 
-      <label for="psw"><b>Password</b></label>
-      <input type="password" placeholder="Enter Password" name="psw" required>
-        <br><br>
-      <button class="btn btn-button bg-primary" type="submit">Login</button><br><br>
 
-      <a href=""><button class="btn btn-button bg-danger"><span style="background-color:blue;"><i class="bx bxl-facebook"></span></i>Facebook</button></a><br><br>
-
-    <a href=""><button class="btn btn-button bg-warning"><span style="background-color:white;"><i class="bx bxl-google"></i></span>Google</button></a>
+<!--Technician  Modal-->
+<div class="modal fade" id="customer_login" tabindex="-1" role="dialog"  aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h3 class="modal-title text-black">Get Professional Technicians</h3>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="card text-center">
+          <div class="card-header bg-warning">
+           Customer Login
+          </div>
+          <div class="card-body">
+            <form>
+              <div class="form-group text-left">
+                <label for="customer_email">Email address</label>
+                <input type="email" class="form-control" id="customer_email" aria-describedby="emailHelp" placeholder="Enter email" name="customer_email">
+              </div>
+              <div class="form-group text-left">
+                <label for="customer_password">Password</label>
+                <input type="password" class="form-control" id="customer_password" placeholder="Enter Password" name="customer_password">
+              </div>
+              <div class="form-check text-left">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+              </div>
+              <button type="submit" class="btn btn-success" style="margin-top: -50px;" >Login</button>
+            </form> 
+            <div class="text-center">
+               <a href=""><button type="button" class="btn btn-primary"><i class="bx bxl-google mr-2"></i><span>|</span><span class="m-3">Google Login </span></button></a>  
+            </div>
+          </div>
+          <div class="card-footer text-muted p-3 bg-warning">
+            <div class="text-left">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+            </div>
+            <div class="text-right" style="margin-top:-30px;">
+               <span class="text-danger">Forgot <a style="color:white;" href="#">password?</a></span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="container" style="background-color:#f1f1f1">
-      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-      <span class="psw">Forgot <a href="#">password?</a></span>
-    </div>
-  </form>
+  </div>
 </div>
 
-<!--  technician login     -->
-<div id="technician" class="modal">
-  <form class="modal-content animate" action="/action_page.php" method="post">
-    <div class="imgcontainer">
-      <span onclick="document.getElementById('technician').style.display='none'" class="close" title="Close Modal">&times;</span>
-      <img src="img_avatar2.png" alt="Avatar" class="avatar">
-      <h2>Get a Bussiness</h2>
+<div class="modal fade" id="technician_login" tabindex="-1" role="dialog"  aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h3 class="modal-title text-black">Get a Bussiness</h3>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="card text-center">
+          <div class="card-header bg-warning">
+           Technician Login
+          </div>
+          <div class="card-body">
+            <form>
+              <div class="form-group text-left">
+                <label for="technician_email">Email address</label>
+                <input type="email" class="form-control" id="technician_email" aria-describedby="emailHelp" placeholder="Enter email" name="technician_email">
+              </div>
+              <div class="form-group text-left">
+                <label for="technician_password">Password</label>
+                <input type="password" class="form-control" id="technician_password" placeholder="Password" name="technician_password">
+              </div>
+              <div class="form-check text-left">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+              </div>
+              <button type="submit" class="btn btn-success" style="margin-top: -50px;" >Login</button>
+            </form> 
+            <div class="text-center">
+               <a href=""><button type="button" class="btn btn-primary"><i class="bx bxl-google mr-2"></i><span>|</span><span class="m-3">Google Login </span></button></a>  
+            </div>
+          </div>
+          <div class="card-footer text-muted p-3 bg-warning">
+            <div class="text-left">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+            </div>
+            <div class="text-right" style="margin-top:-30px;">
+               <span class="text-danger">Forgot <a style="color:white;" href="#">password?</a></span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="container">
-      <label for="uname"><b>Username</b></label>
-      <input type="text" placeholder="Enter Username" name="uname" required>
-
-      <label for="psw"><b>Password</b></label>
-      <input type="password" placeholder="Enter Password" name="psw" required>
-        <br><br>
-      <button class="btn btn-button bg-primary" type="submit">Login</button><br><br>
-
-      <a href=""><button class="btn btn-button bg-danger"><span style="background-color:blue;"><i class="bx bxl-facebook"></span></i>Facebook</button></a><br><br>
-
-    <a href=""><button class="btn btn-button bg-warning"><span style="background-color:white;"><i class="bx bxl-google"></i></span>Google</button></a>
-    </div>
-    <div class="container" style="background-color:#f1f1f1">
-      <button type="button" onclick="document.getElementById('technician').style.display='none'" class="cancelbtn">Cancel</button>
-      <span class="psw">Forgot <a href="#">password?</a></span>
-    </div>
-  </form>
+  </div>
 </div>
 
-    <!-- ======= Footer ======= -->
+<!-- ======= Footer ======= -->
   <footer id="footer">
     <div class="container">
       <h3>HTH</h3>
